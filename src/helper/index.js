3aed62e775d35c0225f7b0d69a8083fa4e62a950
Jefferson de Oliveira Silva <spalla69@gmail.com>
2017-09-13 11:50:47 -0300

    FIX the deepMerge method (#16)
    * FIX the deepmerge method
    
    * FIX the deepMerge method and change the remove ticket method
    
    * CHANGE the release version
    
    * CHANGE docs
    
    * FIX unit test

--- src/helper/index.js
+++ src/helper/index.js
@@ -5,8 +5,8 @@
 */
 export var deepMerge = (target, source) => {
     for (let key of Object.keys(source)) {
-        if (source[key] instanceof Object) {
-            Object.assign(source[key], deepMerge(target[key], source[key]));
+        if (source[key] instanceof Object && !source[key].hasOwnProperty('length')) {
+            Object.assign(source[key], deepMerge(target[key] || {}, source[key]));
         }
     }
 
