09b383fe5725716d2290428fd7ae001072487a59
Udimberto S. Junior <junior.silvestre@ingresse.com>
2018-03-14 13:48:03 -0300

    Add: 'ingresse.setAuth' method

---
+++ dist/script/patch-for-local.js
@@ -0,0 +1,10 @@
+'use strict';
+
+(function () {
+  if (location.protocol === 'file:') {
+    var elms = document.querySelectorAll('a[href="./"]');
+    for (var i = 0; i < elms.length; i++) {
+      elms[i].href = './index.html';
+    }
+  }
+})();
