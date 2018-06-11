0a721e665cc8205d9b64fe4081f7d429bc1ca9f1
new69 <spalla69@gmail.com>
2018-06-11 17:37:30 -0300

    GENETATE docs

--- dist/apis/api.user.js
+++ dist/apis/api.user.js
@@ -117,7 +117,6 @@ var ApiUser = exports.ApiUser = function (_RequestHandler) {
             var data = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
             var query = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
 
-            query.method = 'create';
             return this.post('/user/', data, query);
         }
 
